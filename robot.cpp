de2821ddef75e3f042dc175392e31f4ac8a5bcc2
Thayiebah <156815902+thayiebah@users.noreply.github.com>
2024-06-10 17:05:20 +0800

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,6 +1,8 @@
 #include "robot.h"
+#include "battlefield.h"
+using namespace std;
 
-Robot::Robot(int x, int y, std::string name, char symbol, Battlefield* battlefield) 
+Robot::Robot(int x, int y,string name, char symbol, Battlefield* battlefield) 
     : posX(x), posY(y), name(name), symbol(symbol), battlefield(battlefield), lives(3) {}
 
 void Robot::takeTurn() {
@@ -9,3 +11,20 @@ void Robot::takeTurn() {
     move();
     fire(posX, posY);
 }
+
+void Robot::look (int x, int y)
+{
+    court << name << "is looking around at position (" << x << ", " << y << ")." << endl;
+}
+
+void Robot::move()
+{
+    posX += 1 // move to the right
+    court << name << " moved to position (" << posX << ", " << posY << ")." << endl;
+}
+
+void Robot::fire (int x, int y) 
+{
+    court << name << " fires at position (" << x << ", " << y << ")." << endl;
+}
+    
